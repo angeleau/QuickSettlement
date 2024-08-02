9051b123a6bbb1016b6e2f9d72d835ae3f071183
Testo McTestFace <test.test@test.com>
2024-08-02 14:23:29 +0200

    Project Setup

---
+++ Source/QuickSettlement/QuickSettlement.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "QuickSettlement.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuickSettlement, "QuickSettlement" );
